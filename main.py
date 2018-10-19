7e35ec49cab8672f8fa66a11c84e034d273caa40
Bartosz Górka <bartosz.gorka@interia.eu>
2018-10-19 21:03:56 +0200

    Simulate days and choice visit hotel (when visit - also which) or not

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 NUMBER_OF_PEOPLE = 10000
 PROBABILITY = 0.1
 NUMBER_OF_HOTELS = 100
@@ -5,7 +7,16 @@ NUMBER_OF_DAYS = 100
 
 
 def main():
-    pass
+    # We should simulate daily choice
+    for day in range(NUMBER_OF_DAYS):
+        print(f'DAY {day}:')
+        # We should simulate each person
+        for person_id in range(NUMBER_OF_PEOPLE):
+            # Check random decision - visit hotel or not
+            if np.random.random_sample() < PROBABILITY:
+                # Guest decided visit hotel - should check which
+                hotel_id = np.random.randint(0, NUMBER_OF_HOTELS)
+                print(f'Guest {person_id} visit {hotel_id}')
 
 
 if __name__ == '__main__':
