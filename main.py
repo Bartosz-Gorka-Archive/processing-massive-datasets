c72b0f2aa7058ac33d818891cb93f7399748250d
Bartosz Górka <bartosz.gorka@interia.eu>
2018-10-20 07:08:17 +0200

    Add iterations to enable calculations stats inside program (avg, min, max, median)

--- main.py
+++ main.py
@@ -6,61 +6,73 @@ NUMBER_OF_PEOPLE = 10000
 PROBABILITY = 0.1
 NUMBER_OF_HOTELS = 100
 NUMBER_OF_DAYS = 100
+NUMBER_OF_STATS_ITERATIONS = 2
 
 
 def main():
-    # Dict - pairs
-    potential_terrorist_pairs = {}
+    # Loop with stats - variables
+    unique_terrorist_cardinality_list = []
+    terrorist_days_cardinality_list = []
+    meetings_stats_list = []
 
-    # Dict - statistics
-    meetings_stats = {}
-    terrorist_days = 0
-    unique_terrorist = set()
+    for iteration in range(NUMBER_OF_STATS_ITERATIONS):
+        # Dict - pairs
+        potential_terrorist_pairs = {}
 
-    # We should simulate daily choice
-    for day in range(NUMBER_OF_DAYS):
-        # Dict - visit hotels in selected days
-        visits_in_hotels = {}
+        # Dict - statistics
+        meetings_stats = {}
+        terrorist_days = 0
+        unique_terrorist = set()
 
-        # We should simulate each person
-        for person_id in range(NUMBER_OF_PEOPLE):
-            # Check random decision - visit hotel or not
-            if np.random.random_sample() < PROBABILITY:
-                # Guest decided visit hotel - should check which
-                hotel_id = np.random.randint(0, NUMBER_OF_HOTELS)
+        # We should simulate daily choice
+        for day in range(NUMBER_OF_DAYS):
+            # Dict - visit hotels in selected days
+            visits_in_hotels = {}
 
-                # Append person_id to guest ids in selected hotel
-                guest_ids = visits_in_hotels.get(hotel_id, [])
-                guest_ids.append(person_id)
-                visits_in_hotels.update({hotel_id: guest_ids})
+            # We should simulate each person
+            for person_id in range(NUMBER_OF_PEOPLE):
+                # Check random decision - visit hotel or not
+                if np.random.random_sample() < PROBABILITY:
+                    # Guest decided visit hotel - should check which
+                    hotel_id = np.random.randint(0, NUMBER_OF_HOTELS)
 
-        # Make combinations to connect person in pair
-        for (hotel_id, guest_ids) in visits_in_hotels.items():
-            guests_combinations = list(itertools.combinations(guest_ids, 2))
+                    # Append person_id to guest ids in selected hotel
+                    guest_ids = visits_in_hotels.get(hotel_id, [])
+                    guest_ids.append(person_id)
+                    visits_in_hotels.update({hotel_id: guest_ids})
 
-            # Update counters in pairs dict
-            for (guest, partner) in guests_combinations:
-                key = f'{guest}-{partner}'
-                together_days = potential_terrorist_pairs.get(key, 0)
-                potential_terrorist_pairs.update({key: together_days + 1})
+            # Make combinations to connect person in pair
+            for (hotel_id, guest_ids) in visits_in_hotels.items():
+                guests_combinations = list(itertools.combinations(guest_ids, 2))
 
-    # Calculate statistics
-    for (pair, cardinality) in potential_terrorist_pairs.items():
-        meetings_stats.update({cardinality: meetings_stats.get(cardinality, 0) + 1})
-        if cardinality >= 2:
-            terrorist_days += len(list(itertools.combinations(range(cardinality), 2)))
-            for person_id in pair.split('-'):
-                unique_terrorist.add(person_id)
+                # Update counters in pairs dict
+                for (guest, partner) in guests_combinations:
+                    key = f'{guest}-{partner}'
+                    together_days = potential_terrorist_pairs.get(key, 0)
+                    potential_terrorist_pairs.update({key: together_days + 1})
 
-    # Order histogram stats
-    ordered_stats = collections.OrderedDict(sorted(meetings_stats.items()))
+        # Calculate statistics
+        for (pair, cardinality) in potential_terrorist_pairs.items():
+            meetings_stats.update({cardinality: meetings_stats.get(cardinality, 0) + 1})
+            if cardinality >= 2:
+                terrorist_days += len(list(itertools.combinations(range(cardinality), 2)))
+                for person_id in pair.split('-'):
+                    unique_terrorist.add(person_id)
 
-    # Display results
-    print(f'Terrorist * day = {terrorist_days}')
-    print(f'Unique terrorist count = {len(unique_terrorist)}')
-    print('Histogram stats')
-    for (key, val) in ordered_stats.items():
-        print(f'\t{key}: {val}')
+        # Order histogram stats
+        ordered_stats = collections.OrderedDict(sorted(meetings_stats.items()))
+
+        # Display results
+        print(f'Terrorist * day = {terrorist_days}')
+        print(f'Unique terrorist count = {len(unique_terrorist)}')
+        print('Histogram stats')
+        for (key, val) in ordered_stats.items():
+            print(f'\t{key}: {val}')
+
+        # Append values from current iteration to main stats params
+        unique_terrorist_cardinality_list.append(len(unique_terrorist))
+        terrorist_days_cardinality_list.append(terrorist_days)
+        meetings_stats_list.append(ordered_stats)
 
 
 if __name__ == '__main__':
