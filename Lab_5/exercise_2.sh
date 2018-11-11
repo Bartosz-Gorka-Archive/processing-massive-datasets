441ea9bc6e447dedee558b0cf33b662e7914cf95
Bartosz Górka <bartosz.gorka@interia.eu>
2018-11-11 20:48:11 +0100

    Use formatted samples file instead raw in exercise 2

--- Lab_5/exercise_2.sh
+++ Lab_5/exercise_2.sh
@@ -6,7 +6,7 @@ END {
   for (key in listen) {
     print key " " length(listen[key]) > "ex_2.tmp"
   }
-}' triplets_sample_20p.txt
+}' samples_formatted.txt
 
 sort --numeric-sort -k2 -r ex_2.tmp | head -n 10
 rm ex_2.tmp
