38d68ff1ea0b6af11743acbf1c518f3b8fe7bc53
Bartosz Górka <bartosz.gorka@interia.eu>
2019-01-05 18:14:47 +0100

    Use BitArray in project

--- Lab_10/exercise_10.py
+++ Lab_10/exercise_10.py
@@ -1,30 +1,34 @@
 import csv
+from bitstring import BitArray
 from collections import Counter
 
-SOURCE_FILE_NAME = 'facts.csv'
+SOURCE_FILE_NAME = 'facts2.csv'
 NEAREST_NEIGHBOR_SIZE = 100
+MAX_SONG_ID = 999056 + 1  # TODO replace
 
 
-def jaccard(list_a, list_b):
-    # List stored 'hit' in song_id - no zeros in both records
-    # When value in one or both lists - we are sure this value can be used in Jaccard Index
-    all_elements_list = list_a + list_b
-    elements_cardinality = Counter(all_elements_list).values()
+def jaccard(bitarray_a, bitarray_b):
+    min_one_of = bitarray_a.copy()
+    min_one_of |= bitarray_b
 
-    total_objects = len(elements_cardinality)
-    the_same = len(list(filter(lambda x: x == 2, elements_cardinality)))
+    both = bitarray_a.copy()
+    both &= bitarray_b
+
+    total_objects = min_one_of.count(True)
 
     if total_objects > 0:
-        return the_same / total_objects
+        return both.count(True) / total_objects
     else:
         return 0.0
 
 
-def calculate_similarity(similarity, songs, user_id, my_song_ids):
-    # Run in loop Jaccard calculations - skip our record
-    for (partner_user_id, partner_songs_list) in songs.items():
-        if partner_user_id != user_id:
-            similarity_value = jaccard(my_song_ids, partner_songs_list)
+def calculate_similarity(similarity, songs, max_user_id):
+    for (user_id, my_song_list) in songs.items():
+        print(user_id)
+        for partner_user_id in range(user_id + 1, max_user_id + 1):
+            partner_songs_list = songs.get(partner_user_id, [])
+            similarity_value = jaccard(my_song_list, partner_songs_list)
+            print(partner_user_id)
 
             # Set my similarity with partner - his/her stats
             partner_similarity_list = similarity.get(partner_user_id, [])
@@ -32,10 +36,9 @@ def calculate_similarity(similarity, songs, user_id, my_song_ids):
             similarity.update({partner_user_id: partner_similarity_list})
 
             # Similarity - my stats
-            if user_id <= 100:
-                my_similarity_list = similarity.get(user_id, [])
-                my_similarity_list.append([partner_user_id, similarity_value])
-                similarity.update({user_id: my_similarity_list})
+            my_similarity_list = similarity.get(user_id, [])
+            my_similarity_list.append([partner_user_id, similarity_value])
+            similarity.update({user_id: my_similarity_list})
 
 
 def sort_by_similarity(similarity_list):
@@ -45,10 +48,6 @@ def sort_by_similarity(similarity_list):
 def nearest_neighbors(similarity):
     f = open('stats.txt', 'a')
     for (user_id, list_of_partners_similarity) in similarity.items():
-        # TODO This is only for eliminate point 3: Calculations for all users in file
-        if user_id > 100:
-            continue
-
         f.write(f'User = {user_id}\n')
         f.write('{:8d} 1.00000\n'.format(user_id))
         for record in sort_by_similarity(list_of_partners_similarity)[0:NEAREST_NEIGHBOR_SIZE]:
@@ -79,25 +78,21 @@ def main():
                 user_songs_ids.append(song_id)
             else:
                 # New user - we must store list of song ids, run calculations and start new group
-                # To speed up calculations we should make songs unique
                 if previous_user_id != 0:
-                    user_songs_ids = list(set(user_songs_ids))
-                    # TODO This is only for eliminate point 3: Calculations for all users in file
-                    if user_id <= 100:
-                        user_songs_groups.update({previous_user_id: user_songs_ids})
-                    calculate_similarity(user_similarity, user_songs_groups, previous_user_id, user_songs_ids)
+                    bitmap = BitArray(MAX_SONG_ID)
+                    bitmap.set(True, user_songs_ids)
+                    user_songs_groups.update({previous_user_id: bitmap})
 
+                print(user_id)
                 previous_user_id = user_id
                 user_songs_ids = [song_id]
 
-        # To speed up calculations we should make songs unique
-        user_songs_ids = list(set(user_songs_ids))
-        # TODO This is only for eliminate point 3: Calculations for all users in file
-        if previous_user_id <= 100:
-            user_songs_groups.update({previous_user_id: user_songs_ids})
-        calculate_similarity(user_similarity, user_songs_groups, previous_user_id, user_songs_ids)
+        bitmap = BitArray(MAX_SONG_ID)
+        bitmap.set(True, user_songs_ids)
+        user_songs_groups.update({previous_user_id: bitmap})
 
-        print('FINAL')
+        print('BUILT!')
+        calculate_similarity(user_similarity, user_songs_groups, previous_user_id)
         nearest_neighbors(user_similarity)
 
 
