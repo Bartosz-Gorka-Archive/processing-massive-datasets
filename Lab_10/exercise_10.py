a226d9d68dc3765a63a2bd7aabdeea8f9b347bfb
Bartosz Górka <bartosz.gorka@interia.eu>
2019-01-06 21:38:22 +0100

    Rewrite Exercise 10

--- Lab_10/exercise_10.py
+++ Lab_10/exercise_10.py
@@ -1,7 +1,7 @@
 import csv
 from numpy import sum as np_sum
 
-SOURCE_FILE_NAME = 'facts.csv'
+SOURCE_FILE_NAME = 'facts3.csv'
 NEAREST_NEIGHBOR_SIZE = 100
 
 
@@ -25,25 +25,18 @@ def jaccard(list_a, list_b):
     return intersection_count / (length_list_a + length_list_b - intersection_count)
 
 
-def calculate_similarity(similarity, songs, max_user_id):
+def calculate_similarity(similarity, songs):
     for (user_id, my_song_list) in songs.items():
         if user_id > 100:
-            continue
-        print(user_id)
+            break
+
         my_similarity_list = similarity.get(user_id, [])
 
-        for partner_user_id in range(user_id + 1, max_user_id + 1):
-            partner_songs_list = songs.get(partner_user_id, [])
+        for (partner_id, partner_songs_list) in songs.items():
             similarity_value = jaccard(my_song_list, partner_songs_list)
 
-            if partner_user_id <= 100:
-                # Set my similarity with partner - his/her stats
-                partner_similarity_list = similarity.get(partner_user_id, [])
-                partner_similarity_list.append([user_id, similarity_value])
-                similarity[partner_user_id] = partner_similarity_list
-
-            # Similarity - my stats
-            my_similarity_list.append([partner_user_id, similarity_value])
+            if similarity_value > 0:
+                my_similarity_list.append([partner_id, similarity_value])
 
         # Store updated stats
         similarity[user_id] = my_similarity_list
@@ -58,11 +51,10 @@ def nearest_neighbors(similarity):
     for user_id in sorted(similarity.keys()):
         list_of_partners_similarity = similarity[user_id];
         if user_id > 100:
-            continue
+            break
 
         f.write(f'User = {user_id}\n')
-        f.write('{:8d} 1.00000\n'.format(user_id))
-        [f.write('{:8d} {:7.5f}\n'.format(record[0], record[1])) for record in sort_by_similarity(list_of_partners_similarity)[0:NEAREST_NEIGHBOR_SIZE-1] if record[1] > 0]
+        [f.write('{:8d} {:7.5f}\n'.format(record[0], record[1])) for record in sort_by_similarity(list_of_partners_similarity)[0:NEAREST_NEIGHBOR_SIZE-1]]
 
     f.close()
 
@@ -76,34 +68,21 @@ def main():
         user_songs_groups = {}
         user_similarity = {}
 
-        previous_user_id = 0
-        user_songs_ids = []
-
+        print('START')
         for record in reader:
             # Fist value in record is a `user_id`, second - `song_id`
             user_id = int(record[0])
             song_id = int(record[1])
 
-            # If previous user is the same - append song to list
-            if user_id == previous_user_id:
-                user_songs_ids.append(song_id)
+            # Sets for speed-up calculations
+            if user_id in user_songs_groups:
+                user_songs_groups[user_id].add(song_id)
             else:
-                # New user - we must store list of song ids, run calculations and start new group
-                if previous_user_id != 0:
-                    user_songs_ids = list(set(user_songs_ids))
-                    user_songs_ids.sort()
-                    user_songs_groups[previous_user_id] = user_songs_ids
+                user_songs_groups[user_id] = {song_id}
 
-                previous_user_id = user_id
-                user_songs_ids = [song_id]
-
-        user_songs_ids = list(set(user_songs_ids))
-        user_songs_ids.sort()
-        user_songs_groups[previous_user_id] = user_songs_ids
-
-        print('BUILT!')
-        calculate_similarity(user_similarity, user_songs_groups, previous_user_id)
-        print('STORE!')
+        print('CALCULATE SIMILARITY')
+        calculate_similarity(user_similarity, user_songs_groups)
+        print('NEIGHBORS')
         nearest_neighbors(user_similarity)
         print('FINISH')
 
